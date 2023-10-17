d2d978c47407b2b321020cffaab222c4395ff7e9
Ankur Panthri <panthriankur@outlook.com>
2023-10-17 17:01:10 +0530

    Bug Fixes

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -6,7 +6,7 @@ const handler = NextAuth({
         GoogleProvider({
             clientId: process.env.CLIENT_ID,
             clientSecret: process.env.CLIENT_SECRET,
-            authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
+            // authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
         }),
     ],
     // jwt:{
